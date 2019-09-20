1c49750527f5b65e6f43973b78811ceed36cf746
ayerest <irisiseve@gmail.com>
2019-09-20 15:04:33 -0700

    finished react dq  props and state lab

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -12,6 +12,9 @@ export default class BabyHog extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      weight: 0
+    }
   }
 
   changeWeight = (e) => {
@@ -23,22 +26,41 @@ export default class BabyHog extends Component {
   }
 
   render() {
+    let { id, name, hobby } = this.props.piglet
+    let imgSrc = ""
+    switch(this.props.eyeColor) {
+      case("blue"):
+        imgSrc = BlueBaby
+        break;
+      case("sun"):
+        imgSrc = SunBaby;
+        break;
+      case("glowing"):
+        imgSrc = GlowingBaby;
+        break;
+      default:
+        imgSrc = normalBaby;
+        break;
+    }
+   
+     
+
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
+        <h1>{name}</h1>
+        <h3>Weight: {this.state.weight}</h3>
+        <h3>Hobby: {hobby}</h3>
+        <h4>Eye Color: {this.props.eyeColor} </h4>
           
-        <Button name="+">
+        <Button name="+" onClick={this.changeWeight}>
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button name="-" onClick={this.changeWeight}>
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={imgSrc} style={{height: '200px'}} alt="MasterBlasterJrJr" />
         </div>
         
       </li>
